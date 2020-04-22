b0889588cf5a163c27f7792fd925aec8781d23b2
unknown <DimaBalaushko@gmail.com>
2020-04-22 16:04:20 +0300

    Initial commit

---
+++ Project/MyProject/ModuleClass/src/Class/Package/Person.java
@@ -0,0 +1,25 @@
+package Class.Package;
+
+public class Person {
+    // Instance variables
+    private String name;
+    private String address;
+
+    // Constructor
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+    public String getAddress() {
+        return address;
+    }
+
+    public String toString() {
+        return name + "(" + address + ")";
+    }
+}
